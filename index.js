e1721630fb070d4e1049f838d932d5203dc9baab
Jhon Michael Babadilla <125885842+JMBabadilla@users.noreply.github.com>
2023-03-26 10:10:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,3 +29,28 @@ for (let i = 0; i < fLen; i++) {
   text2 +=  + i + ":" + items[i] + "<br>";
 }
 document.getElementById("demo2").innerHTML = text2;
+
+//activity 4
+let array1 = ['apple', 'banana', 'mango'];
+let array2 = ['orange', 'ponkan', 'melon'];
+
+document.getElementById("demo3").innerHTML = array2[1];
+
+//activity 5
+let menu = ['Cinnamon Doughnuts', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+let index = prompt("Enter the index of the item to replace:");
+menu[index] = "Sunny Side up";
+document.getElementById("demo4").innerHTML = menu;
+
+//activity 6
+function Contact(name, number) {
+    this.name = name;
+    this.number = number;
+  
+    this.print = function() {
+        document.getElementById("demo4").innerHTML = this.name + ": " + this.number;
+
+    }
+  }
+  let contact = new Contact("Michelley", "09123456789");
+  contact.print();
